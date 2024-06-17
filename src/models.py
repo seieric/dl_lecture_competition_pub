97156ba09689754a517cc66fc630c2c637875a11
seieric <30688855+seieric@users.noreply.github.com>
2024-06-17 14:10:43 +0900

    add ResNet34 based model

--- src/models.py
+++ src/models.py
@@ -18,6 +18,32 @@ class ResNet152(nn.Module):
         X = X.unsqueeze(2)        
         return self.resnet152(X)
 
+class ResNet34(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1d = nn.Conv1d(271, 64, kernel_size=3, stride=1, padding=1)
+        self.resnet34 = models.resnet34()
+        self.resnet34.conv1 = nn.Conv2d(64, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.resnet34.fc = nn.Linear(self.resnet34.fc.in_features, 1854)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = self.conv1d(X)
+        X = X.unsqueeze(2)
+        return self.resnet34(X)
+    
+class ResNet152(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1d = nn.Conv1d(271, 64, kernel_size=3, stride=1, padding=1)
+        self.resnet152 = models.resnet152()
+        self.resnet152.conv1 = nn.Conv2d(64, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.resnet152.fc = nn.Linear(self.resnet152.fc.in_features, 1854)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = self.conv1d(X)
+        X = X.unsqueeze(2)        
+        return self.resnet152(X)
+
 class BasicConvClassifier(nn.Module):
     def __init__(
         self,
