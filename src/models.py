38f25a3b4ccb74a262c370df30c79c4e1046786a
seieric <30688855+seieric@users.noreply.github.com>
2024-06-23 00:52:31 +0900

    update: use ResNet18 based model

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ import math
 import os
 
 from .layers.subject import SubjectLayer
-from .layers.resnet_gelu.resnet34 import ResNet34GELU
+from .layers.resnet_gelu.resnet18 import ResNet18GELU
 
 
 class MyModel(nn.Module):
@@ -15,7 +15,7 @@ class MyModel(nn.Module):
         super().__init__()
         self.conv1d = nn.Conv1d(271, 271, kernel_size=1, stride=1, padding=1)
         self.subject_layer = SubjectLayer(4, 271)
-        self.classifier = ResNet34GELU(in_channels=271, num_classes=1854, dropout=0.3)
+        self.classifier = ResNet18GELU(in_channels=271, num_classes=1854, dropout=0.3)
 
     def forward(self, X: torch.Tensor, subject_idx: torch.Tensor) -> torch.Tensor:
         X = self.conv1d(X)
