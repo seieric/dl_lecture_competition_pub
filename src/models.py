6c33396dc16367e8fabe8075d20512d598a929b9
seieric <30688855+seieric@users.noreply.github.com>
2024-06-22 21:04:54 +0900

    update: trying simple convs with gelu activation

--- src/models.py
+++ src/models.py
@@ -17,27 +17,14 @@ class ResNet34(nn.Module):
         self.conv1d = nn.Conv1d(271, 271, kernel_size=1, stride=1, padding=1)
         self.dropout1 = nn.Dropout(0.5)
         self.subject_layer = SubjectLayer(4, 271)
-        if pretrained:
-            self.resnet34 = models.resnet34(
-                weights=models.ResNet34_Weights.IMAGENET1K_V1
-            )
-            for i, param in enumerate(self.resnet34.parameters()):
-                if i >= num_freezed_params:
-                    param.requires_grad = True
-        else:
-            self.resnet34 = models.resnet34()
-        self.resnet34.conv1 = nn.Conv2d(
-            271, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
-        )
-        self.resnet34.fc = nn.Linear(self.resnet34.fc.in_features, 1854)
+        self.classifier = BasicConvClassifier(1854, 281, 271)
 
     def forward(self, X: torch.Tensor, subject_idx: torch.Tensor) -> torch.Tensor:
-        X = self.spatial_attention(X)
         X = self.conv1d(X)
-        X = self.dropout1(X)
+        # X = self.dropout1(X)
         X = self.subject_layer(X, subject_idx)
-        X = X.unsqueeze(2)
-        return self.resnet34(X)
+        # X = X.unsqueeze(2)
+        return self.classifier(X)
 
 
 class ResNet50(nn.Module):
@@ -73,20 +60,21 @@ class ResNet152(nn.Module):
 
 
 class BasicConvClassifier(nn.Module):
-    def __init__(
-        self, num_classes: int, seq_len: int, in_channels: int, hid_dim: int = 128
-    ) -> None:
+    def __init__(self, num_classes: int, seq_len: int, in_channels: int) -> None:
         super().__init__()
 
         self.blocks = nn.Sequential(
-            ConvBlock(in_channels, hid_dim),
-            ConvBlock(hid_dim, hid_dim),
+            ConvBlock(in_channels, 256, 256, dilation_k=1),
+            ConvBlock(256, 256, 256, dilation_k=2),
+            ConvBlock(256, 256, 512, dilation_k=3),
+            ConvBlock(512, 512, 512, dilation_k=4),
+            ConvBlock(512, 512, 512, dilation_k=5),
         )
 
         self.head = nn.Sequential(
             nn.AdaptiveAvgPool1d(1),
             Rearrange("b d 1 -> b d"),
-            nn.Linear(hid_dim, num_classes),
+            nn.Linear(512, num_classes),
         )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
@@ -105,36 +93,50 @@ class ConvBlock(nn.Module):
     def __init__(
         self,
         in_dim,
+        hid_dim,
         out_dim,
+        dilation_k,
         kernel_size: int = 3,
-        p_drop: float = 0.1,
+        p_drop: float = 0.5,
     ) -> None:
         super().__init__()
 
         self.in_dim = in_dim
+        self.hid_dim = hid_dim
         self.out_dim = out_dim
 
-        self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same")
-        self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same")
-        # self.conv2 = nn.Conv1d(out_dim, out_dim, kernel_size) # , padding="same")
+        self.conv0 = nn.Conv1d(
+            in_dim,
+            hid_dim,
+            kernel_size,
+            dilation=2 ** (2 * dilation_k) % 5,
+            padding="same",
+        )
+        self.conv1 = nn.Conv1d(
+            hid_dim,
+            hid_dim,
+            kernel_size,
+            dilation=2 ** (2 * dilation_k + 1) % 5,
+            padding="same",
+        )
+        self.conv2 = nn.Conv1d(
+            hid_dim, out_dim * 2, kernel_size, dilation=2, padding="same"
+        )
 
-        self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
-        self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
+        self.batchnorm0 = nn.BatchNorm1d(num_features=hid_dim)
+        self.batchnorm1 = nn.BatchNorm1d(num_features=hid_dim)
+        self.batchnorm2 = nn.BatchNorm1d(num_features=out_dim * 2)
 
         self.dropout = nn.Dropout(p_drop)
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        if self.in_dim == self.out_dim:
-            X = self.conv0(X) + X  # skip connection
+        if self.in_dim == self.hid_dim:
+            X = F.gelu(self.batchnorm0(self.conv0(X))) + X  # skip connection
         else:
-            X = self.conv0(X)
+            X = F.gelu(self.batchnorm0(self.conv0(X)))
 
-        X = F.gelu(self.batchnorm0(X))
-
-        X = self.conv1(X) + X  # skip connection
-        X = F.gelu(self.batchnorm1(X))
-
-        # X = self.conv2(X)
-        # X = F.glu(X, dim=-2)
+        X = F.gelu(self.batchnorm1(self.conv1(X))) + X  # skip connection
+        X = self.batchnorm2(self.conv2(X))
+        X = F.glu(X, dim=-2)
 
         return self.dropout(X)
