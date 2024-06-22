7dcc082aced1225155c7913d2fbcfe01091e2a33
seieric <30688855+seieric@users.noreply.github.com>
2024-06-23 01:15:02 +0900

    update: パラメータをconfig.yamlに移動・整理

--- main.py
+++ main.py
@@ -50,9 +50,7 @@ def run(args: DictConfig):
     # model = BasicConvClassifier(
     #    train_set.num_classes, train_set.seq_len, train_set.num_channels
     # ).to(device)
-    model = MyModel(
-        pretrained=args.pretrained, num_freezed_params=args.num_freezed_params
-    ).to(device)
+    model = MyModel(dropout=args.dropout).to(device)
     model = torch.nn.DataParallel(model, device_ids=list(range(args.num_gpus)))
 
     # ------------------
