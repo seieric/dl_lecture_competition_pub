43e0348c6bd38dd240f971220981673e542ad443
seieric <30688855+seieric@users.noreply.github.com>
2024-06-26 21:30:27 +0900

    update: add blocks, change alpha value, etc

--- src/layers/megnet.py
+++ src/layers/megnet.py
@@ -16,13 +16,11 @@ class MEGNet(nn.Module):
                 num_channels,
                 256,
                 kernel_size=(1, 64),
-                padding=(0, 64 // 2),
+                padding=(0, 32),
                 bias=False,
             ),
-            # (8, 1, 283)
             nn.BatchNorm2d(256),
             nn.Conv2d(256, 512, kernel_size=(1, 16), groups=256, bias=False),
-            # (16, 1, 268)
             nn.BatchNorm2d(512),
             nn.ELU(),
             nn.AvgPool2d((1, 4)),
@@ -30,29 +28,36 @@ class MEGNet(nn.Module):
         )
         # shape: (N, 512, 1, 67)
         self.block2 = nn.Sequential(
-            nn.Conv2d(512, 1024, kernel_size=(1, 8), padding=(0, 8), bias=False),
-            # (N, 16, 1, 75)
+            nn.Conv2d(512, 512, kernel_size=(1, 8), padding=(0, 8), bias=False),
+            nn.BatchNorm2d(512),
+            nn.ELU(),
+            nn.AvgPool2d((1, 4)),
+            dropoutFunc(dropout),
+        )
+        # shape: (N, 512, 1, 16)
+        self.block3 = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=(1, 4), padding=(0, 0), bias=False),
             nn.BatchNorm2d(1024),
             nn.ELU(),
             nn.AvgPool2d((1, 4)),
             dropoutFunc(dropout),
         )
-        # shape: (N, 1024, 1, 18)
-        self.block3 = nn.Sequential(
-            nn.Conv2d(1024, 2048, kernel_size=(1, 16), padding=(0, 8), bias=False),
-            # (N, 32, 1, 30)
+        # shape: (N, 1024, 1, 4)
+        self.block4 = nn.Sequential(
+            nn.Conv2d(1024, 2048, kernel_size=(1, 2), padding=(0, 1), bias=False),
             nn.BatchNorm2d(2048),
             nn.ELU(),
-            nn.AvgPool2d((1, 8)),
+            nn.AvgPool2d((1, 2)),
             dropoutFunc(dropout),
         )
         # shape: (N, 2048, 1, 2)
         self.flatten = nn.Flatten()
-        self.dense = nn.Linear(4096, num_classes, bias=False)
+        self.dense = nn.Linear(2048 * 2, num_classes, bias=False)
 
     def forward(self, X):
         X = self.block1(X)
         X = self.block2(X)
         X = self.block3(X)
+        X = self.block4(X)
         X = self.flatten(X)
         return self.dense(X)
