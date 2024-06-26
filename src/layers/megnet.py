2da83eea8269e766ddc31f9038c4ef9dd1b8fb95
seieric <30688855+seieric@users.noreply.github.com>
2024-06-26 15:14:14 +0900

    minor refactor

--- src/layers/megnet.py
+++ src/layers/megnet.py
@@ -6,12 +6,7 @@ class MEGNet(nn.Module):
         self,
         num_classes=1854,
         num_channels=271,
-        seq_len=283,
         dropout=0.5,
-        kernLength=64,
-        F1=256,
-        D=2,
-        F2=512,
         dropoutFunc=nn.Dropout,
     ):
         super(MEGNet, self).__init__()
@@ -19,43 +14,41 @@ class MEGNet(nn.Module):
         self.block1 = nn.Sequential(
             nn.Conv2d(
                 num_channels,
-                F1,
-                kernel_size=(1, kernLength),
-                padding=(0, kernLength // 2),
+                256,
+                kernel_size=(1, 64),
+                padding=(0, 64 // 2),
                 bias=False,
             ),
             # (8, 1, 283)
-            nn.BatchNorm2d(F1),
-            nn.Conv2d(F1, F1 * D, kernel_size=(1, 16), groups=F1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 512, kernel_size=(1, 16), groups=256, bias=False),
             # (16, 1, 268)
-            nn.BatchNorm2d(F1 * D),
+            nn.BatchNorm2d(512),
             nn.ELU(),
             nn.AvgPool2d((1, 4)),
             dropoutFunc(dropout),
         )
         # shape: (N, 512, 1, 67)
         self.block2 = nn.Sequential(
-            nn.Conv2d(F1 * D, F2 * D, kernel_size=(1, 8), padding=(0, 8), bias=False),
+            nn.Conv2d(512, 1024, kernel_size=(1, 8), padding=(0, 8), bias=False),
             # (N, 16, 1, 75)
-            nn.BatchNorm2d(F2 * D),
+            nn.BatchNorm2d(1024),
             nn.ELU(),
             nn.AvgPool2d((1, 4)),
             dropoutFunc(dropout),
         )
         # shape: (N, 1024, 1, 18)
         self.block3 = nn.Sequential(
-            nn.Conv2d(
-                F2 * D, F2 * D * 2, kernel_size=(1, 16), padding=(0, 8), bias=False
-            ),
+            nn.Conv2d(1024, 2048, kernel_size=(1, 16), padding=(0, 8), bias=False),
             # (N, 32, 1, 30)
-            nn.BatchNorm2d(F2 * D * 2),
+            nn.BatchNorm2d(2048),
             nn.ELU(),
             nn.AvgPool2d((1, 8)),
             dropoutFunc(dropout),
         )
-        # shape: (N, 2048, 1, 3)
+        # shape: (N, 2048, 1, 2)
         self.flatten = nn.Flatten()
-        self.dense = nn.Linear(F2 * D * 2 * (seq_len // 128), num_classes, bias=False)
+        self.dense = nn.Linear(4096, num_classes, bias=False)
 
     def forward(self, X):
         X = self.block1(X)
