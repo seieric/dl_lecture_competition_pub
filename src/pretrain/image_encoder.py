895a8766e76ba040bdf3bf91287f41d7e0217dd2
seieric <30688855+seieric@users.noreply.github.com>
2024-07-02 16:47:04 +0900

    fix: supur class init

--- src/pretrain/image_encoder.py
+++ src/pretrain/image_encoder.py
@@ -5,7 +5,7 @@ import torchvision.models as models
 
 class ImageEncoder(nn.Module):
     def __init__(self) -> None:
-        super(self, ImageEncoder).__init__()
+        super(ImageEncoder, self).__init__()
         self.resnet50 = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
         # 最終層を削除して2048次元の特徴量を出力するようにする
         self.resnet50.fc = nn.Identity()
--- src/pretrain/meg_encoder.py
+++ src/pretrain/meg_encoder.py
@@ -6,7 +6,7 @@ from ..layers.megnet import MEGNet
 
 class MEGEncoder(nn.Module):
     def __init__(self, dropout=0) -> None:
-        super().__init__()
+        super(MEGEncoder, self).__init__()
         self.conv1d = nn.Conv1d(271, 271, kernel_size=1, stride=1, padding=1)
         self.subject_layer = SubjectLayer(4, 271)
         self.megnet = MEGNet(dropout=dropout)
