f2fd6eeb5fa271a3936fac29c312da8b3044b216
seieric <30688855+seieric@users.noreply.github.com>
2024-07-02 16:43:30 +0900

    add: ImageEncoder

---
+++ src/pretrain/image_encoder.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class ImageEncoder(nn.Module):
+    def __init__(self) -> None:
+        super(self, ImageEncoder).__init__()
+        self.resnet50 = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+        # 最終層を削除して2048次元の特徴量を出力するようにする
+        self.resnet50.fc = nn.Identity()
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        return self.resnet50(X)
