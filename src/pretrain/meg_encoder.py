99dcea602a71711d1b082c2ba52747ade670f789
seieric <30688855+seieric@users.noreply.github.com>
2024-07-02 17:55:29 +0900

    fix: flatten ouput in MEGEncoder

--- src/pretrain/meg_encoder.py
+++ src/pretrain/meg_encoder.py
@@ -10,8 +10,10 @@ class MEGEncoder(nn.Module):
         self.conv1d = nn.Conv1d(271, 271, kernel_size=1, stride=1, padding=1)
         self.subject_layer = SubjectLayer(4, 271)
         self.megnet = MEGNet(dropout=dropout)
+        self.megnet.dense = nn.Flatten()
 
     def forward(self, X: torch.Tensor, subject_idx: torch.Tensor) -> torch.Tensor:
         X = self.conv1d(X)
         X = self.subject_layer(X, subject_idx)
+        X = X.unsqueeze(2)
         return self.megnet(X)
